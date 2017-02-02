a17186d03b29c01350555ba9b695765d6ec4a896
Alistair Willis <alistairrwillis@gmail.com>
2017-02-02 12:14:52 +0900

    reformat app.js

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
-    var ip = (req.ip).replace(/:+|[a-z]+/g, ""),
+    var ip = (req.ip).replace(/:+|[a-z]+/g, "");
 
-    userAgent = req.get('user-agent'),
-    acceptLanguage = req.get('accept-language'),
+    var userAgent = req.get('user-agent');
+    var acceptLanguage = req.get('accept-language');
 
-    os = parse.parseUserAgent(userAgent),
-    lang = parse.parseAcceptLanguage(acceptLanguage),
+    var os = parse.parseUserAgent(userAgent);
+    var lang = parse.parseAcceptLanguage(acceptLanguage); 
 
-    responseObject = {
+    var responseObject = {
         ip: ip, 
         lang: lang, 
         os: os
-    }
+    }; 
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(responseObject, undefined, 2));
